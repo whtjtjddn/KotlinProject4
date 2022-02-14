54898c9297be101b0d1c7e83b5d5ae96e9e4050d
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-02-14 19:38:38 +0900

    Layout worked out

---
+++ app/src/main/java/com/example/kotlinproject4calculation/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinproject4calculation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun ButtonClicked(v: View){
+
+    }
+    private fun historyButtonClicked(v: View){
+
+    }
+    private fun resultButtonClicked(v: View){
+
+    }
+    private fun clearButtonClicked(v: View){
+
+    }
+}
